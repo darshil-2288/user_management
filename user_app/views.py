9c8037e7fc683b12f2e383e9f4616e8aaf678cfd
darshil-2288 <106592174+darshil-2288@users.noreply.github.com>
2022-09-12 15:11:52 +0530

    Update views.py

--- user_app/views.py
+++ user_app/views.py
@@ -1,36 +1,67 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,HttpResponseRedirect
-from .models import StudentModel
-from user_app.forms import register_form,login_form
-from django.shortcuts import render
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect 
 from django.contrib import messages
-# Create your views here.
+from django.views import View
+from django.contrib.auth.views import LoginView
+from .forms import RegisterForm,LoginForm,UpdateUserForm,UpdateProfileForm
+from django.contrib.auth.decorators import login_required
 
-def register(request):
-	form=register_form()
-	if request.method=='POST':
-		form=register_form(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('login')
+def home(request):
+	return render(request, 'home.html')
+
+class RegisterView(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to access the register page while logged in
+        if request.user.is_authenticated:
+        	return redirect(to='/')
+
+        # else process dispatch as it otherwise normally would
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
+
+# Class based view that extends from the built in login view to add a remember me functionality
+class CustomLoginView(LoginView):
+	form_class = LoginForm
+	def form_valid(self, form_class):
+		remember_me = form_class.cleaned_data.get('remember_me')
+		if not remember_me:
+		    # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+		    self.request.session.set_expiry(0)
+		    # Set session as modified to force data updates/cookie to be saved.
+		    self.request.session.modified = True
+		# else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+		return super(CustomLoginView, self).form_valid(form_class)
+
+@login_required
+def profile(request):
+	if request.method == 'POST':
+		user_form = UpdateUserForm(request.POST, instance=request.user)
+		profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, 'Your profile is updated successfully')
+			return redirect(to='users-profile')
 	else:
-		form=register_form()
-	return render(request,'register.html',{'form':form})
-def login(request):
-		if request.method == 'POST':
-			form = AuthenticationForm(request=request, data=request.POST)
-			if form.is_valid():
-				username = form.cleaned_data.get('username')
-				password = form.cleaned_data.get('password')
-				user = authenticate(username=username, password=password)
-				if user is not None:
-					print(user)
-					
-					return redirect('/home')
-				else:
-					print('User not found')
-		else:
-			form=AuthenticationForm()
-		return render(request, 'login.html', {'form': form})		
+		user_form = UpdateUserForm(instance=request.user)
+		profile_form = UpdateProfileForm(instance=request.user.profile)
+	return render(request, 'profile.html', {'user_form': user_form, 'profile_form': profile_form})		
