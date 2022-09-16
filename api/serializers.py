1f0c8f87e339737482762050ac5113be3193aee1
darshil-2288 <106592174+darshil-2288@users.noreply.github.com>
2022-09-16 16:29:51 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,77 +1,20 @@
-
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from rest_framework import serializers
-from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from .models import userProfile
-User = get_user_model()
-
-
-
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-
-    email = serializers.EmailField(
-        required=True,
-        label="Email Address"
-    )
-
-    password = serializers.CharField(
-        required=True,
-        label="Password",
-        
-    )
-
-   
-
-    first_name = serializers.CharField(
-        required=True
-    )
-
-    last_name = serializers.CharField(
-        required=True
-    )
-
-   
-
-    class Meta(object):
-        model = User
-        fields = ['username', 'email', 'password', 'first_name', 'last_name']
-   
-        
-
-class UserLoginSerializer(serializers.ModelSerializer):
-
-	    username = serializers.CharField(
-	        required=True,
-	        
-	        write_only=True,
-	    )
-
-	    email = serializers.EmailField(
-	        required=True,
-	        
-	        write_only=True,
-	        label="Email Address"
-	    )
-
-	    
-
-	    password = serializers.CharField(
-	        required=True,
-	        write_only=True,
-	        style={'input_type': 'password'}
-	    )
-
-	    class Meta(object):
-	        model = User
-	        fields = ['email', 'username', 'password']
-
+	class Meta:
+		model=User
+		fields=['username','email','password']
 
 
 class userProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model=userProfile
         fields=['id','location','description']
+
 
 
 
