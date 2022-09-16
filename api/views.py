2088bb061a9df1114d73dc9ae44d6aa1052a25dd
darshil-2288 <106592174+darshil-2288@users.noreply.github.com>
2022-09-16 16:31:03 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,47 +1,32 @@
-from django.contrib.auth import get_user_model
-from rest_framework.response import Response
 from . import serializers
-from rest_framework import status,generics,views
-from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
-from .models import userProfile
-from .serializers import userProfileSerializer
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser
-import io
+from rest_framework import generics, permissions, status, views
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
-from django.shortcuts import HttpResponse
+from .models import userProfile
 
-
-
-User=get_user_model()
 class UserRegistrationAPIView(generics.CreateAPIView):
-	"""
-	Endpoint for user registration.
-	"""
-
-
+	permission_classes = (permissions.AllowAny, )
 	serializer_class = serializers.UserRegistrationSerializer
 	queryset = User.objects.all()
+	for user in queryset:
+		Token.objects.get_or_create(user=user)
 
 class UserLoginAPIView(views.APIView):
-	"""
-	Endpoint for user login. Returns authentication token on success.
-	"""
-
-	serializer_class = serializers.UserLoginSerializer
-
-	def post(self, request):
-		serializer = self.serializer_class(data=request.data)
-		if serializer.is_valid(raise_exception=True):
-			return Response(serializer.data, status=status.HTTP_200_OK)
-
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+		permission_classes = (permissions.AllowAny, )
+		serializer_class = serializers.UserRegistrationSerializer
+		def post(self, request):
+			serializer = self.serializer_class(data=request.data)
+			if serializer.is_valid(raise_exception=True):
+				return Response(serializer.data, status=status.HTTP_200_OK)
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def create_profile(request):
 	queryset=userProfile.objects.all()
-	serializer=userProfileSerializer(data=request.data)
+	serializer=serializers.userProfileSerializer(data=request.data)
 	if serializer.is_valid():
 		serializer.save()
 	return Response(serializer.data)
@@ -49,16 +34,6 @@ def create_profile(request):
 @api_view(['GET'])
 def detail_view(request,pk):
 	profile=userProfile.objects.get(id=pk)
-	serializer=userProfileSerializer(profile,many=False)
-	return Response(serializer.data)
-
-
-
-
-
-    
-
-
-    
-
+	serializer=serializers.userProfileSerializer(profile,many=False)
+	return Response(serializer.data)			
 
