5c14c294d1b30f79866b7aabcd756ec243772c27
darshil-2288 <106592174+darshil-2288@users.noreply.github.com>
2022-09-16 16:30:15 +0530

    Update signals.py

--- api/signals.py
+++ api/signals.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-from .models import userProfile
-
-User=get_user_model()
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
-        userProfile.objects.create(user=instance)
+        Token.objects.create(user=instance)
